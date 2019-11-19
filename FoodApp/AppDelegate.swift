3a066630bfdf64667cccd45c5e39f45f2f061bb1
Dmitriy Petrov <dmitriy.petrov@bytepace.com>
2019-11-19 13:19:01 +0600

    initial commit

---
+++ FoodApp/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  FoodApp
+//
+//  Created by Dmitriy Petrov on 19/11/2019.
+//  Copyright © 2019 BytePace. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+}
---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '11.0'
+
+target 'FoodApp' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for FoodApp
+  pod 'RxSwift'
+  pod 'RxDataSources'
+
+end
